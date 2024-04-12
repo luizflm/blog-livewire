6685434bb7f52707d7f0bdc5e673adb70d230923
Luiz Felipe <lepinholima@gmail.com>
2024-04-12 11:14:37 -0300

    fix: set nullable to image column from posts table

--- database/migrations/2024_04_12_133345_create_posts_table.php
+++ database/migrations/2024_04_12_133345_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
