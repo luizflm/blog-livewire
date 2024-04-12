f10b0c3597a2f65709d74708240a0b56129a49d4
Luiz Felipe <lepinholima@gmail.com>
2024-04-12 15:39:55 -0300

    refactor: remove category and tag seeders from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class,
-            TagSeeder::class,
             PostSeeder::class
         ]);
     }
