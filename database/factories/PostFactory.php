8fc0f2f64ca0e5813ea38a4afd4f725a36b94d1a
Luiz Felipe <lepinholima@gmail.com>
2024-04-12 11:32:35 -0300

    feat: create posts table factory and seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->text,
+            'image' => $this->faker->imageUrl(width: 480, height: 320, category: 'post', randomize: true, word: 'post', format: 'jpg'),
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
