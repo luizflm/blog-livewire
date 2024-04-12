2c647211b343d8d01dcace34ba001f57c6d82969
Luiz Felipe <lepinholima@gmail.com>
2024-04-12 11:39:56 -0300

    feat: add many to many relationship to posts and tags tables seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,9 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::factory()->count(10)->create();
+        Post::factory()
+            ->has(Tag::factory()->count(3))
+            ->count(10)
+            ->create();
     }
 }
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,9 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        Tag::factory()->count(10)->create();
+        Tag::factory()
+            ->has(Post::factory()->count(3))
+            ->count(10)
+            ->create();
     }
 }
